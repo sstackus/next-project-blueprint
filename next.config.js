41ec6aada0b33ce42d315a963f1b82584cc7859b
Simon Stack <simon@stackus.dev>
2020-01-17 17:17:12 +0000

    Add next config, ESLint support, more dependencies

---
+++ next.config.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-param-reassign, import/no-extraneous-dependencies */
+const withCss = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
+const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
+const path = require('path');
+
+function cwd(...fragments) {
+  return path.resolve(__dirname, ...fragments);
+}
+
+module.exports = withCss(withSass({
+  target: 'server',
+  webpack(config, { webpack, dev }) {
+    config.resolve.alias['@/images'] = cwd('public', 'images');
+    config.resolve.alias['@'] = cwd('src');
+
+    config.plugins.push(
+      new webpack.EnvironmentPlugin(Object.keys(process.env)),
+    );
+
+    config.resolve.plugins.push(
+      new DirectoryNamedWebpackPlugin(true),
+    );
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      loader: '@svgr/webpack',
+      options: { icon: true },
+    });
+
+    config.module.rules.push({
+      test: /\.jsx?$/,
+      loader: 'eslint-loader',
+      exclude: [cwd('node_modules'), cwd('.next')],
+      enforce: 'pre',
+      options: {
+        emitWarning: dev,
+      },
+    });
+
+    return config;
+  },
+  sassLoaderOptions: {
+    includePaths: [cwd('node_modules'), cwd('src')],
+  },
+}));
