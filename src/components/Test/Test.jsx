c9f77456066dcd2a9c429e3e90e96f1121b735b4
Simon Stack <simon@stackus.dev>
2020-01-17 18:30:14 +0000

    Update `Test` to demonstrate features more clearly

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -4,7 +4,7 @@ import { bind } from '@/decorators';
 
 export default class Test extends React.PureComponent {
   state = {
-    val: '',
+    val: 'Test',
   };
 
   @bind
@@ -16,14 +16,22 @@ export default class Test extends React.PureComponent {
     const { val } = this.state;
 
     return (
-      <div>
+      <div className="border border-gray-200 p-4">
         <input
           name="val"
           type="text"
           value={val}
           onChange={this.handleChange}
-          className="border border-gray-300 py-2 px-4 focus:bg-gray-100 outline-none"
+          className="border border-gray-300 py-2 px-3 focus:bg-gray-100 outline-none"
         />
+
+        <p className="mt-4 mx-3">
+          State:
+          <br />
+          <code className="bg-gray-100 border p-2 mt-2 inline-block">
+            {JSON.stringify(this.state)}
+          </code>
+        </p>
       </div>
     );
   }
